7d1486bbca396a85195dd5622baa47e6c8f6dae6
Hendrik <hendrik_naether@web.de>
2013-06-09 22:05:09 +0200

    Parser umgeschrieben Teil 1
    - Parser unterteilt in ChannelParser und FeedItemParser
    - ChannelParser komplett neu geschrieben

---
+++ CrossmediaDemo/ChannelParser.h
@@ -0,0 +1,13 @@
+//
+//  ChannelParser.h
+//  CrossmediaDemo
+//
+//  Created by Hendrik Näther on 09.06.13.
+//  Copyright (c) 2013 Hendrik Näther. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ChannelParser : NSObject
+
+@end
---
+++ CrossmediaDemo/ChannelParser.m
@@ -0,0 +1,13 @@
+//
+//  ChannelParser.m
+//  CrossmediaDemo
+//
+//  Created by Hendrik Näther on 09.06.13.
+//  Copyright (c) 2013 Hendrik Näther. All rights reserved.
+//
+
+#import "ChannelParser.h"
+
+@implementation ChannelParser
+
+@end
